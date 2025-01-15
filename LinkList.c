4d54beef10663f33fc8bc0ffc1d2da45b0c12b6d
Raihan Ahmed <raiihan2500@gmail.com>
2025-01-16 00:44:32 +0600

    LinkedList with C

--- LinkList.c
+++ LinkList.c
@@ -1,62 +1,89 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node{
+struct Node{
     int data;
-    
-    struct node* next;
+    struct Node* next, *prev;
 };
 
-typedef struct node* nodeptr;
+typedef struct Node* nodeptr;
 
 nodeptr getNode(){
-    nodeptr new_node = (nodeptr)malloc(sizeof(struct node));
+    nodeptr new_node = (nodeptr) malloc(sizeof(struct Node));
+    new_node->next = NULL;
+    new_node->prev = NULL;
     return new_node;
 }
 
-
-struct Linklist{
+struct LinkedList{
     nodeptr head;
+    nodeptr tail;
 };
 
-void addFront(struct Linklist* list, int data){
+void addFront(struct LinkedList *list, int data){
     nodeptr new_node = getNode();
     new_node->data = data;
+    
+    if(!list->head){
+        list->head = list->tail = new_node;
+        return;
+    }
+    list->head->prev = new_node;
     new_node->next = list->head;
     list->head = new_node;
 }
 
-void deleteFront(struct Linklist* list){
-    if(list->head == NULL){
-        printf("List is empty\n");
+void addBack(struct LinkedList *list, int data){
+    nodeptr new_node = getNode();
+    new_node->data = data;
+    if(list->tail == NULL){
+        list->head = list->tail = new_node;
         return;
     }
+    
+    new_node->prev = list->tail;
+    list->tail->next = new_node;
+    list->tail = new_node;
+}
 
+void deleteFront(struct LinkedList *list){
+    if(list->head == NULL)return;
     nodeptr temp = list->head;
     list->head = list->head->next;
+    list->head->prev = NULL;
     free(temp);
 }
 
-void displayList(struct Linklist* list){
+void deleteBack(struct LinkedList *list){
+    if(list->tail == NULL)return;
+    nodeptr temp = list->tail;
+    list->tail = list->tail->prev;
+    list->tail->next = NULL;
+    free(temp);
+}
+
+void display(struct LinkedList *list){
     nodeptr temp = list->head;
-    while(temp != NULL){
+    while(temp){
         printf("%d ", temp->data);
-        temp = temp->next;        
+        temp = temp->next;
     }
     printf("\n");
 }
 
-
 __int32_t main(){
-    struct Linklist list; list.head = NULL;
-
+    struct LinkedList list;
     addFront(&list, 5);
     addFront(&list, 10);
-    addFront(&list, 15);
+    display(&list);
 
-    displayList(&list);
+    addBack(&list, 15);
+    addFront(&list, 7);
+    display(&list);
 
     deleteFront(&list);
-    displayList(&list);
-
+    display(&list);
+    
+    deleteBack(&list);
+    display(&list);
 }
